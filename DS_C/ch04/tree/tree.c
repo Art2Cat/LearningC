13db2e783a993552fdc8745079f2ca4884e5b54f
art2cat <yiming.whz@gmail.com>
2018-03-11 18:34:53 +0800

    format with google-cpp-style

--- DS_C/ch04/tree/tree.c
+++ DS_C/ch04/tree/tree.c
@@ -1,6 +1,6 @@
 #include "tree.h"
-#include <stdlib.h>
 #include "fatal.h"
+#include <stdlib.h>
 
 struct TreeNode {
   ElementType Element;
@@ -21,31 +21,38 @@ SearchTree MakeEmpty(SearchTree T) {
 
 /* START: fig4_18.txt */
 Position Find(ElementType X, SearchTree T) {
-  if (T == NULL) return NULL;
-  if (X < T->Element)
+  if (T == NULL) {
+    return NULL;
+  }
+  if (X < T->Element) {
     return Find(X, T->Left);
-  else if (X > T->Element)
+  } else if (X > T->Element) {
     return Find(X, T->Right);
-  else
+  } else {
     return T;
+  }
 }
 /* END */
 
 /* START: fig4_19.txt */
 Position FindMin(SearchTree T) {
-  if (T == NULL)
+  if (T == NULL) {
     return NULL;
-  else if (T->Left == NULL)
+  } else if (T->Left == NULL) {
     return T;
-  else
+  } else {
     return FindMin(T->Left);
+  }
 }
 /* END */
 
 /* START: fig4_20.txt */
 Position FindMax(SearchTree T) {
-  if (T != NULL)
-    while (T->Right != NULL) T = T->Right;
+  if (T != NULL) {
+    while (T->Right != NULL) {
+      T = T->Right;
+    }
+  }
 
   return T;
 }
@@ -53,24 +60,26 @@ Position FindMax(SearchTree T) {
 
 /* START: fig4_22.txt */
 SearchTree Insert(ElementType X, SearchTree T) {
-   if (T == NULL) {
+  if (T == NULL) {
     /* Create and return a one-node tree */
-     T = malloc(sizeof(struct TreeNode));
-     if (T == NULL)
-       FatalError("Out of space!!!");
-    else {
-       T->Element = X;
-       T->Left = T->Right = NULL;
+    T = (SearchTree)malloc(sizeof(struct TreeNode));
+    if (T == NULL) {
+      FatalError("Out of space!!!");
+    } else {
+      T->Element = X;
+      T->Left = T->Right = NULL;
     }
-  } else
-       if (X < T->Element)
-     T->Left = Insert(X, T->Left);
-  else
-       if (X > T->Element)
-     T->Right = Insert(X, T->Right);
-  /* Else X is in the tree already; we'll do nothing */
-
-   return T; /* Do not forget this line!! */
+  } else {
+    if (X < T->Element) {
+      T->Left = Insert(X, T->Left);
+    } else {
+      if (X > T->Element) {
+        T->Right = Insert(X, T->Right);
+      }
+      /* Else X is in the tree already; we'll do nothing */
+    }
+  }
+  return T; /* Do not forget this line!! */
 }
 /* END */
 
@@ -78,26 +87,29 @@ SearchTree Insert(ElementType X, SearchTree T) {
 SearchTree Delete(ElementType X, SearchTree T) {
   Position TmpCell;
 
-  if (T == NULL)
+  if (T == NULL) {
     Error("Element not found");
-  else if (X < T->Element) /* Go left */
+  } else if (X < T->Element) { /* Go left */
     T->Left = Delete(X, T->Left);
-  else if (X > T->Element) /* Go right */
+  } else if (X > T->Element) { /* Go right */
     T->Right = Delete(X, T->Right);
-  else                         /* Found element to be deleted */
-      if (T->Left && T->Right) /* Two children */
-  {
-    /* Replace with smallest in right subtree */
-    TmpCell = FindMin(T->Right);
-    T->Element = TmpCell->Element;
-    T->Right = Delete(T->Element, T->Right);
-  } else /* One or zero children */
-  {
-    TmpCell = T;
-    if (T->Left == NULL) /* Also handles 0 children */
-      T = T->Right;
-    else if (T->Right == NULL)
-      T = T->Left;
+  } else { /* Found element to be deleted */
+           /* Two children */
+    if (T->Left && T->Right) {
+      /* Replace with smallest in right subtree */
+      TmpCell = FindMin(T->Right);
+      T->Element = TmpCell->Element;
+      T->Right = Delete(T->Element, T->Right);
+    } else {
+      /* One or zero children */
+      TmpCell = T;
+      if (T->Left == NULL) {
+        /* Also handles 0 children */
+        T = T->Right;
+      } else if (T->Right == NULL) {
+        T = T->Left;
+      }
+    }
     free(TmpCell);
   }
 
